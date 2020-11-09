2411886d3533906878f037d93a366b58c45832b2
Courtney Carson <cmacarson@gmail.com>
2020-11-09 12:28:53 -0800

    react-router

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
 import LandingPage from './01.Landing/LandingPage';
 import LogIn from './02.LogIn/LogIn';
-import HowTo from './06.HowTo/HowTo';
 import SignUp from './03.SignUp/SignUp';
 import HomePage from './04.Home/HomePage';
+import AboutPage from './05.About/AboutPage';
+import HowTo from './06.HowTo/HowTo';
+import TriggerPoint from './07.TriggerPoint/TriggerPoint';
+import PastTreatments from './08.PastTreatments/PastTreatments';
 
 class App extends Component {
   render() {
@@ -14,14 +17,24 @@ class App extends Component {
         <Router>
           <Link to='/'>LandingPage</Link>
           <Link to='/log-in'>Log In</Link>
-          <Link to='/home'>Home</Link>
-          <Link to='/how-to'>Treatment</Link>
           <Link to='/sign-up'>Sign Up</Link>
+          <Link to='/home'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/how-to'>Treatment</Link>
+          <Link to='/trigger-point'>Trigger Point</Link>
+          <Link to='/past-treatments'>Past Treatments</Link>
+
+          
           <Route path='/' component={LandingPage} />
           <Route path="/log-in" component={LogIn} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/how-to" component={HowTo} />
           <Route path="/sign-up" component={SignUp} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/how-to" component={HowTo} />
+          <Route path="/trigger-point" component={TriggerPoint} />
+          <Route path="/past-treatments" component={PastTreatments} />
+
+
         </Router>
   
       </div>
