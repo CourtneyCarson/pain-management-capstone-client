89997efb5a9bd5566125b5e67cdfa39a64b7400f
Courtney Carson <cmacarson@gmail.com>
2020-11-20 19:28:11 -0800

    fixed landing page styling

--- src/01.Landing/LandingPage.js
+++ src/01.Landing/LandingPage.js
@@ -5,19 +5,16 @@ export default function LandingPage(props) {
   return (
 
     <div className='landing-page'>
-
-      <section>
-        <header>
-          <h3>Get to the root of your pain</h3>
-          <img src="https://live.staticflickr.com/65535/50607295132_069ed53912_w.jpg" alt="SubOccipital" />
-        </header>
-
-        <p>
-          This is a troubleshooting application for you to seek out a solution for your pain.
-          Log in to learn more about Trigger Points and how to treat them.
+      <div className='landing-page-header'>
+        <h2>Get To The Root Of Your Pain</h2>
+        <img src="https://live.staticflickr.com/65535/50607295132_069ed53912_w.jpg" alt="SubOccipital" />
+      </div>
+      <div className='landing-page-content'>
+        <p> This is a troubleshooting application for you to seek out a solution for your pain.</p>
+        <p> Log in to learn more about Trigger Points and how to treat them.
           </p>
-      </section>
-    </div>
+      </div>
 
+    </div>
   )
 }
--- src/04.Home/HomePage.js
+++ src/04.Home/HomePage.js
@@ -12,44 +12,37 @@ export default function HomePage(props) {
       </header>
 
       <section>
-        {/* <h5>Select The Area You Are Feeling Pain</h5> */}
-        {/* <!-- images of areas of pain - as buttons --> */}
+
         <div className='tp-button-links'>
           <Link to="/trigger-point/1">
             <img src="https://live.staticflickr.com/65535/50618518676_0b0b8a8b4e_w.jpg"
               alt="referral pattern for sub occipitals" />
           </Link>
+
           <Link to="/trigger-point/2">
             <img src="https://live.staticflickr.com/65535/50609334773_21b30a4971_w.jpg"
               alt="referral pattern masseter" />
           </Link>
+
           <Link to="/trigger-point/3">
             <img src="https://live.staticflickr.com/65535/50618983602_45e45bb0fc_w.jpg"
               alt="referral pattern levator scapulae" />
           </Link>
+
           <Link to="/trigger-point/4">
             <img src="https://live.staticflickr.com/65535/50609334608_22029f7b3d_w.jpg"
               alt="referral pattern middle trapezius" />
           </Link>
 
-
-          {/* extra links for padding  */}
           <Link to="/trigger-point/5">
             <img src="https://live.staticflickr.com/65535/50617716018_02c89f7993_w.jpg"
               alt="referral patter iliocostalis" />
           </Link>
+
           <Link to="/trigger-point/6">
             <img src="https://live.staticflickr.com/65535/50609334823_2c272797d4_w.jpg"
               alt="referral pattern glute medius" />
           </Link>
-           <Link to="/trigger-point/7">
-            <img src="https://live.staticflickr.com/65535/50622815271_f9ac2d43f5_w.jpg"
-              alt="referral pattern glute medius" />
-          </Link>
-          {/* <Link to="/trigger-point/8">
-            <img src="https://live.staticflickr.com/65535/50609334823_a1b1177ff0_w.jpg"
-              alt="referral pattern glute medius" />
-          </Link> */}
 
         </div>
       </section>
