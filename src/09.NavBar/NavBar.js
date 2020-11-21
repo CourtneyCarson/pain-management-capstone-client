bc330b2388da04750b1a73ef20295e9ef461d043
Courtney Carson <cmacarson@gmail.com>
2020-11-20 16:03:50 -0800

    color palette updates

--- src/09.NavBar/NavBar.js
+++ src/09.NavBar/NavBar.js
@@ -8,84 +8,77 @@ export default class NavBar extends React.Component {
     return (
       <header className='nav-bar'>
         <nav className='nav-link'>
-          <div className='nav-bar'>
-          <Route exact path={['/', '/log-out']} render={() => {
-            return (
-              <>
-                <Link to='/log-in'>Log In</Link>
-                <Link to='/sign-up'>Sign Up</Link>
-              </>
-            )
+          {/* <div className='nav-words'> */}
+            <Route exact path={['/', '/log-out']} render={() => {
+              return (
+                <>
+                  <Link to='/log-in'>Log In</Link>
+                  <Link to='/sign-up'>Sign Up</Link>
+                </>
+              )
             }} />
-            {/* log in page */}
-            <Route exact path="/log-in" render={() => {
-            return (
-              <>
-                <Link to='/home'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/how-to'>How To</Link>
-                <Link to='/log-out'>Log Out</Link>
-              </>
-            )
-          }} />
+           
+            <Route exact path={['/log-in', '/sign-up']} render={() => {
+              return (
+                <>
+                  <Link to='/'>Home</Link>
+                </>
+              )
+            }} />
 
+            <Route exact path="/home" render={() => {
+              return (
+                <>
+                  <Link to='/about'>About</Link>
+                  <Link to='/how-to'>How To</Link>
+                  <Link to='/past-treatments'>Account</Link>
+                  <Link to='/log-out'>Log Out</Link>
+                </>
+              )
+            }} />
+            <Route exact path="/about" render={() => {
+              return (
+                <>
+                  <Link to='/home'>Home</Link>
+                  <Link to='/how-to'>How To</Link>
+                  <Link to='/past-treatments'>Account</Link>
+                  <Link to='/log-out'>Log Out</Link>
+                </>
+              )
+            }} />
 
+            <Route exact path="/how-to" render={() => {
+              return (
+                <>
+                  <Link to='/home'>Home</Link>
+                  <Link to='/about'>About</Link>
+                  <Link to='/past-treatments'>Account</Link>
+                  <Link to='/log-out'>Log Out</Link>
+                </>
+              )
+            }} />
 
+            <Route exact path="/trigger-point/:id" render={() => {
+              return (
+                <>
+                  <Link to='/home'>Home</Link>
+                  <Link to='/past-treatments'>Account</Link>
+                  <Link to='/log-out'>Log Out</Link>
+                </>
+              )
+            }} />
 
-
-          <Route exact path="/home" render={() => {
-            return (
-              <>
-                <Link to='/about'>About</Link>
-                <Link to='/how-to'>How To</Link>
-                <Link to='/past-treatments'>Account</Link>
-                <Link to='/log-out'>Log Out</Link>
-              </>
-            )
-          }} />
-          <Route exact path="/about" render={() => {
-            return (
-              <>
-                <Link to='/home'>Home</Link>
-                <Link to='/how-to'>How To</Link>
-                <Link to='/past-treatments'>Account</Link>
-                <Link to='/log-out'>Log Out</Link>
-              </>
-            )
-          }} />
-
-          <Route exact path="/how-to" render={() => {
-            return (
-              <>
-                <Link to='/home'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/past-treatments'>Account</Link>
-                <Link to='/log-out'>Log Out</Link>
-              </>
-            )
-          }} />
-
-          <Route exact path="/trigger-point/:id" render={() => {
-            return (
-              <>
-                <Link to='/home'>Home</Link>
-                <Link to='/past-treatments'>Account</Link>
-                <Link to='/log-out'>Log Out</Link>
-              </>
-            )
-          }} />
-
-          <Route exact path="/past-treatments" render={() => {
-            return (
-              <>
-                <Link to='/home'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/how-to'>How To</Link>
-                <Link to='/log-out'>Log Out</Link>
-              </>
-            )
-          }} />
-</div>
+            <Route exact path="/past-treatments" render={() => {
+              return (
+                <>
+                  <Link to='/home'>Home</Link>
+                  <Link to='/about'>About</Link>
+                  <Link to='/how-to'>How To</Link>
+                  <Link to='/log-out'>Log Out</Link>
+                </>
+              )
+            }} />
+          {/* </div> */}
         </nav>
       </header>
     )
