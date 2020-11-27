28f3d8711e4c9c3a7166552f5f027cebb26890cd
Courtney Carson <cmacarson@gmail.com>
2020-11-27 11:48:29 -0800

    readme api doc

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // API_ENDPOINT: 'https://dry-bastion-25929.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://dry-bastion-25929.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   // API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'tp-capstone-auth-token',
 };
